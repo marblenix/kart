cc0c878a71a6217d98f28fc7a971185777003dda
Caleb Marble <marblenix@users.noreply.github.com>
2020-04-02 14:15:37 -0500

    upgrade yarn packages
    cleanup package.json and eslint

--- src/App.vue
+++ src/App.vue
@@ -25,11 +25,11 @@ export default {
 </template>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
-    margin-top: 3.75em;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 3.75em;
+}
 </style>
--- src/components/Config.vue
+++ src/components/Config.vue
@@ -2,12 +2,12 @@
 import { BListGroup, BListGroupItem } from 'bootstrap-vue';
 
 /**
-   * The following distributions are used by version 4.1 of Mario Kart 8 to
-   * determine the probability of obtaining a certain item when an Item Box
-   * is opened.
-   *
-   * The probability of each item is coded as a percentage multiplied by two.
-   */
+ * The following distributions are used by version 4.1 of Mario Kart 8 to
+ * determine the probability of obtaining a certain item when an Item Box
+ * is opened.
+ *
+ * The probability of each item is coded as a percentage multiplied by two.
+ */
 export default {
   name: 'Config',
   components: {
@@ -17,150 +17,161 @@ export default {
 
   data: function () {
     return {
-      defaults: [{
-        distance: 400,
-        current: 'Banana',
-        items: [
-          { id: 'Coin', probability: 70 },
-          { id: 'Banana', probability: 65 },
-          { id: 'GreenShell', probability: 50 },
-          { id: 'Mushroom', probability: 5 },
-          { id: 'RedShell', probability: 5 },
-          { id: 'SuperHorn', probability: 5 }
-        ]
-      }, {
-        distance: 1000,
-        current: 'Banana',
-        items: [
-          { id: 'RedShell', probability: 50 },
-          { id: 'GreenShell', probability: 25 },
-          { id: 'Mushroom', probability: 20 },
-          { id: 'Banana', probability: 20 },
-          { id: 'TripleBanana', probability: 15 },
-          { id: 'Coin', probability: 15 },
-          { id: 'TripleGreenShells', probability: 10 },
-          { id: 'PiranhaPlantPot', probability: 10 },
-          { id: 'FireFlower', probability: 10 },
-          { id: 'BombOmb', probability: 10 },
-          { id: 'TripleRedShells', probability: 5 },
-          { id: 'SuperHorn', probability: 5 },
-          { id: 'BoomerangFlower', probability: 5 }
-        ]
-      }, {
-        distance: 2000,
-        current: 'Banana',
-        items: [
-          { id: 'RedShell', probability: 30 },
-          { id: 'Mushroom', probability: 25 },
-          { id: 'GreenShell', probability: 20 },
-          { id: 'TripleRedShells', probability: 15 },
-          { id: 'TripleMushroom', probability: 15 },
-          { id: 'PiranhaPlantPot', probability: 15 },
-          { id: 'BombOmb', probability: 15 },
-          { id: 'TripleGreenShells', probability: 10 },
-          { id: 'TripleBanana', probability: 10 },
-          { id: 'FireFlower', probability: 10 },
-          { id: 'BoomerangFlower', probability: 10 },
-          { id: 'Banana', probability: 10 },
-          { id: 'Crazy8', probability: 5 },
-          { id: 'SuperHorn', probability: 5 },
-          { id: 'Coin', probability: 5 }
-        ]
-      }, {
-        distance: 3300,
-        current: 'Blooper',
-        items: [
-          { id: 'TripleMushroom', probability: 60 },
-          { id: 'Mushroom', probability: 50 },
-          { id: 'RedShell', probability: 20 },
-          { id: 'GreenShell', probability: 15 },
-          { id: 'TripleRedShells', probability: 10 },
-          { id: 'TripleGreenShells', probability: 10 },
-          { id: 'BoomerangFlower', probability: 10 },
-          { id: 'Crazy8', probability: 5 },
-          { id: 'PiranhaPlantPot', probability: 5 },
-          { id: 'FireFlower', probability: 5 },
-          { id: 'BombOmb', probability: 5 },
-          { id: 'Blooper', probability: 5 }
-        ]
-      }, {
-        distance: 5500,
-        current: 'Blooper',
-        items: [
-          { id: 'TripleMushroom', probability: 85 },
-          { id: 'Mushroom', probability: 30 },
-          { id: 'Star', probability: 25 },
-          { id: 'GoldenMushroom', probability: 25 },
-          { id: 'RedShell', probability: 10 },
-          { id: 'BulletBill', probability: 10 },
-          { id: 'Crazy8', probability: 5 },
-          { id: 'SpinyShell', probability: 5 },
-          { id: 'Blooper', probability: 5 }
-        ]
-      }, {
-        distance: 8000,
-        current: 'BulletBill',
-        items: [
-          { id: 'TripleMushroom', probability: 65 },
-          { id: 'Star', probability: 40 },
-          { id: 'GoldenMushroom', probability: 40 },
-          { id: 'BulletBill', probability: 30 },
-          { id: 'Mushroom', probability: 10 },
-          { id: 'Crazy8', probability: 5 },
-          { id: 'SpinyShell', probability: 5 },
-          { id: 'LightningBolt', probability: 5 }
-        ]
-      }, {
-        distance: 13000,
-        current: 'BulletBill',
-        items: [
-          { id: 'BulletBill', probability: 60 },
-          { id: 'GoldenMushroom', probability: 55 },
-          { id: 'TripleMushroom', probability: 35 },
-          { id: 'Star', probability: 35 },
-          { id: 'LightningBolt', probability: 10 },
-          { id: 'SpinyShell', probability: 5 }
-        ]
-      }, {
-        distance: 26000,
-        current: 'BulletBill',
-        items: [
-          { id: 'BulletBill', probability: 85 },
-          { id: 'GoldenMushroom', probability: 60 },
-          { id: 'Star', probability: 30 },
-          { id: 'LightningBolt', probability: 15 },
-          { id: 'TripleMushroom', probability: 10 }
-        ]
-      }, {
-        distance: 999998,
-        current: 'BulletBill',
-        items: [
-          { id: 'BulletBill', probability: 70 },
-          { id: 'GoldenMushroom', probability: 60 },
-          { id: 'Star', probability: 40 },
-          { id: 'TripleMushroom', probability: 30 }
-        ]
-      }, {
-        distance: 999999,
-        current: 'BulletBill',
-        items: [
-          { id: 'BulletBill', probability: 70 },
-          { id: 'GoldenMushroom', probability: 60 },
-          { id: 'Star', probability: 40 },
-          { id: 'TripleMushroom', probability: 30 }
-        ]
-      }]
+      defaults: [
+        {
+          distance: 400,
+          current: 'Banana',
+          items: [
+            { id: 'Coin', probability: 70 },
+            { id: 'Banana', probability: 65 },
+            { id: 'GreenShell', probability: 50 },
+            { id: 'Mushroom', probability: 5 },
+            { id: 'RedShell', probability: 5 },
+            { id: 'SuperHorn', probability: 5 }
+          ]
+        },
+        {
+          distance: 1000,
+          current: 'Banana',
+          items: [
+            { id: 'RedShell', probability: 50 },
+            { id: 'GreenShell', probability: 25 },
+            { id: 'Mushroom', probability: 20 },
+            { id: 'Banana', probability: 20 },
+            { id: 'TripleBanana', probability: 15 },
+            { id: 'Coin', probability: 15 },
+            { id: 'TripleGreenShells', probability: 10 },
+            { id: 'PiranhaPlantPot', probability: 10 },
+            { id: 'FireFlower', probability: 10 },
+            { id: 'BombOmb', probability: 10 },
+            { id: 'TripleRedShells', probability: 5 },
+            { id: 'SuperHorn', probability: 5 },
+            { id: 'BoomerangFlower', probability: 5 }
+          ]
+        },
+        {
+          distance: 2000,
+          current: 'Banana',
+          items: [
+            { id: 'RedShell', probability: 30 },
+            { id: 'Mushroom', probability: 25 },
+            { id: 'GreenShell', probability: 20 },
+            { id: 'TripleRedShells', probability: 15 },
+            { id: 'TripleMushroom', probability: 15 },
+            { id: 'PiranhaPlantPot', probability: 15 },
+            { id: 'BombOmb', probability: 15 },
+            { id: 'TripleGreenShells', probability: 10 },
+            { id: 'TripleBanana', probability: 10 },
+            { id: 'FireFlower', probability: 10 },
+            { id: 'BoomerangFlower', probability: 10 },
+            { id: 'Banana', probability: 10 },
+            { id: 'Crazy8', probability: 5 },
+            { id: 'SuperHorn', probability: 5 },
+            { id: 'Coin', probability: 5 }
+          ]
+        },
+        {
+          distance: 3300,
+          current: 'Blooper',
+          items: [
+            { id: 'TripleMushroom', probability: 60 },
+            { id: 'Mushroom', probability: 50 },
+            { id: 'RedShell', probability: 20 },
+            { id: 'GreenShell', probability: 15 },
+            { id: 'TripleRedShells', probability: 10 },
+            { id: 'TripleGreenShells', probability: 10 },
+            { id: 'BoomerangFlower', probability: 10 },
+            { id: 'Crazy8', probability: 5 },
+            { id: 'PiranhaPlantPot', probability: 5 },
+            { id: 'FireFlower', probability: 5 },
+            { id: 'BombOmb', probability: 5 },
+            { id: 'Blooper', probability: 5 }
+          ]
+        },
+        {
+          distance: 5500,
+          current: 'Blooper',
+          items: [
+            { id: 'TripleMushroom', probability: 85 },
+            { id: 'Mushroom', probability: 30 },
+            { id: 'Star', probability: 25 },
+            { id: 'GoldenMushroom', probability: 25 },
+            { id: 'RedShell', probability: 10 },
+            { id: 'BulletBill', probability: 10 },
+            { id: 'Crazy8', probability: 5 },
+            { id: 'SpinyShell', probability: 5 },
+            { id: 'Blooper', probability: 5 }
+          ]
+        },
+        {
+          distance: 8000,
+          current: 'BulletBill',
+          items: [
+            { id: 'TripleMushroom', probability: 65 },
+            { id: 'Star', probability: 40 },
+            { id: 'GoldenMushroom', probability: 40 },
+            { id: 'BulletBill', probability: 30 },
+            { id: 'Mushroom', probability: 10 },
+            { id: 'Crazy8', probability: 5 },
+            { id: 'SpinyShell', probability: 5 },
+            { id: 'LightningBolt', probability: 5 }
+          ]
+        },
+        {
+          distance: 13000,
+          current: 'BulletBill',
+          items: [
+            { id: 'BulletBill', probability: 60 },
+            { id: 'GoldenMushroom', probability: 55 },
+            { id: 'TripleMushroom', probability: 35 },
+            { id: 'Star', probability: 35 },
+            { id: 'LightningBolt', probability: 10 },
+            { id: 'SpinyShell', probability: 5 }
+          ]
+        },
+        {
+          distance: 26000,
+          current: 'BulletBill',
+          items: [
+            { id: 'BulletBill', probability: 85 },
+            { id: 'GoldenMushroom', probability: 60 },
+            { id: 'Star', probability: 30 },
+            { id: 'LightningBolt', probability: 15 },
+            { id: 'TripleMushroom', probability: 10 }
+          ]
+        },
+        {
+          distance: 999998,
+          current: 'BulletBill',
+          items: [
+            { id: 'BulletBill', probability: 70 },
+            { id: 'GoldenMushroom', probability: 60 },
+            { id: 'Star', probability: 40 },
+            { id: 'TripleMushroom', probability: 30 }
+          ]
+        },
+        {
+          distance: 999999,
+          current: 'BulletBill',
+          items: [
+            { id: 'BulletBill', probability: 70 },
+            { id: 'GoldenMushroom', probability: 60 },
+            { id: 'Star', probability: 40 },
+            { id: 'TripleMushroom', probability: 30 }
+          ]
+        }
+      ]
     };
   },
 
   methods: {
-
     /**
-       * Updates the item from a click event
-       * @param event the user-fired click event
-       */
-    updateItem: function (event) {
-      const target = event.currentTarget;
+     * Updates the item from a click event
+     * @param {Event} e the user-fired click event
+     * @returns {void}
+     */
+    updateItem: function (e) {
+      const target = e.currentTarget;
       const distance = target.dataset.distance;
       const items = this.getItemsForDistance(distance);
       this.setItem(distance, this.getItem(items));
@@ -168,19 +179,19 @@ export default {
     },
 
     /**
-       * Converts the item.probability to a percent-friendly field.
-       * @param item the item to return the probability for
-       * @returns {Number} the item probability as a percentage
-       */
+     * Converts the item.probability to a percent-friendly field.
+     * @param {Object} item the item to return the probability for
+     * @returns {Number} the item probability as a percentage
+     */
     toPercent: function (item) {
-      return (item.probability / 2);
+      return item.probability / 2;
     },
 
     /**
-       * Return the total probability of all items in a list. Useful mostly for testing.
-       * @param items an array of items
-       * @returns {Number} the total probability of all items in the list, should always be 100
-       */
+     * Return the total probability of all items in a list. Useful mostly for testing.
+     * @param {Object[]} items an array of items
+     * @returns {Number} the total probability of all items in the list, should always be 100
+     */
     totalProbability: function (items) {
       let probability = 0;
       items.forEach(function (item) {
@@ -190,10 +201,11 @@ export default {
     },
 
     /**
-       * Updates the current item for a given distance for the example roll.
-       * @param distance the distance from first place
-       * @param itemId the id of the item to set as the current
-       */
+     * Updates the current item for a given distance for the example roll.
+     * @param {Number} distance the distance from first place
+     * @param {String} itemId the id of the item to set as the current
+     * @returns {void}
+     */
     setItem: function (distance, itemId) {
       this.defaults.forEach(value => {
         if (value.distance === parseInt(distance)) {
@@ -203,10 +215,10 @@ export default {
     },
 
     /**
-       * Based on the distance from first place, return a list of possible items to draw from.
-       * @param distance the current distance from first place
-       * @returns {Array} a list of items
-       */
+     * Based on the distance from first place, return a list of possible items to draw from.
+     * @param {Number} distance the current distance from first place
+     * @returns {Array} a list of items
+     */
     getItemsForDistance: function (distance) {
       let items = [];
       for (const value of this.defaults) {
@@ -219,10 +231,10 @@ export default {
     },
 
     /**
-       * Based on the probability of an item in a list, get a random item.
-       * @param items the list of items to draw from; expected to have item.probability and item.id
-       * @returns {String} the id of the item randomly chosen
-       */
+     * Based on the probability of an item in a list, get a random item.
+     * @param {Object[]} items the list of items to draw from; expected to have item.probability and item.id
+     * @returns {String} the id of the item randomly chosen
+     */
     getItem: function (items) {
       let total = 0;
       const ranges = [];
@@ -230,7 +242,7 @@ export default {
         ranges.push(item.probability);
       });
       for (let i = 0, len = items.length; i < len; i++) {
-        ranges[i] = [total, total += ranges[i]];
+        ranges[i] = [total, (total += ranges[i])];
       }
       const randomNumber = Math.floor(Math.random() * total);
       for (let i = 0, len = ranges.length; i < len; i++) {
@@ -241,9 +253,10 @@ export default {
     },
 
     /**
-       * Flash an element for a very short while.
-       * @param element the element to blink
-       */
+     * Flash an element for a very short while.
+     * @param {Event} element the element to blink
+     * @returns {void}
+     */
     flash: function (element) {
       let op = 0.1;
       const timer = setInterval(() => {
@@ -254,42 +267,6 @@ export default {
         element.style.filter = 'alpha(opacity=' + op * 100 + ')';
         op += op * 0.1;
       }, 10);
-    },
-
-    bundleCalculator: function (target, ...order) {
-      const phi = 1.6180339887498948482;
-      const len = order.length;
-      const sumFunc = (a, b) => a + b;
-      const round2 = x => Math.round(100 * x) / 100;
-      const roundLargestRemainder = (numbers = [], target = 100) => {
-        const offBy = target - numbers.reduce((acc, x) => acc + round2(x), 0);
-        const length = numbers.length;
-        return numbers
-          .map((v, i) => ({ value: v, index: i }))
-          .sort((a, b) => (round2(a.value) - a.value) - (round2(b.value) - b.value))
-          .map((x, i) => {
-            x.value = round2(x.value) + (offBy > i) - (i >= (length + offBy));
-            return x;
-          })
-          .reduce((acc, x) => {
-            acc[x.index] = x.value;
-            return acc;
-          }, {});
-      };
-
-      if (order.length < len) {
-        order = Array(len).fill().map((_, i) => i);
-      } else if (order.length > len) {
-        order = order.slice(0, len);
-      }
-
-      const ratios = order.map((_, i) => Math.pow(phi, len - i));
-      const ratioSum = ratios.reduce(sumFunc, 0);
-      const rawValues = ratios.map((v, i) => v * target / ratioSum);
-      const values = roundLargestRemainder(rawValues, target);
-      Object.keys(values).forEach((_, i) => {
-        order[i].probability = values[i] * 2;
-      });
     }
   }
 };
@@ -327,9 +304,7 @@ export default {
             :class="i.id"
           >
             {{ i.id }}:
-            <span
-              class="probability"
-            >{{ toPercent(i) }}%</span>
+            <span class="probability">{{ toPercent(i) }}%</span>
           </span>
         </b-list-group-item>
       </b-list-group>
@@ -339,18 +314,13 @@ export default {
 
 <style src="../../public/css/item-images.css"></style>
 <style scoped>
-  .edit-button {
-    text-decoration: underline;
-    cursor: pointer;
-  }
+.example {
+  margin-left: 0.5em;
+  padding: 0.25em;
+  user-select: none;
+}
 
-  .example {
-    margin-left: .5em;
-    padding: .25em;
-    user-select: none;
-  }
-
-  .example:hover {
-    cursor: pointer;
-  }
+.example:hover {
+  cursor: pointer;
+}
 </style>
